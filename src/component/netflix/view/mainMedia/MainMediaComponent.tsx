04a8670f588ef3fa7646526db52e77746d89218a
silverj7 <sej720@naver.com>
2023-11-02 00:16:11 +0900

    - main 버튼 추가

--- src/component/netflix/view/mainMedia/MainMediaComponent.tsx
+++ src/component/netflix/view/mainMedia/MainMediaComponent.tsx
@@ -6,11 +6,17 @@ type Props = {};
 const MainMediaComponent = (props: Props) => {
   return (
     <div className={MainMediaStyle.wrapper}>
-      <div className={MainMediaStyle.ImgWrapper}>
-        <img src="/images/main/mainImg.webp" alt="main" />
-      </div>
-      <div className={MainMediaStyle.ImgWrapper}>
-        <img src="/images/main/mainImg.webp" alt="main" />
+      <div className={MainMediaStyle.mainImgWrapper}>
+        <div
+          className={MainMediaStyle.mainImg}
+          style={{
+            backgroundImage: 'url(/images/main/mainImg.webp)',
+          }}
+        />
+        <div className={MainMediaStyle.playDescWrap}>
+          <button className={MainMediaStyle.playBtn}>재생</button>
+          <button className={MainMediaStyle.descBtn}>상세 정보</button>
+        </div>
       </div>
     </div>
   );
